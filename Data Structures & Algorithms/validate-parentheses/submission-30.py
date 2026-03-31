99fdf9a57b236b890385f4a16fad507e9dcc4429
Rohit Singh <156054570+Rohit1217@users.noreply.github.com>
2026-03-31 16:55:16 +0530

    Add: validate-parentheses - submission-30

---
+++ Data Structures & Algorithms/validate-parentheses/submission-30.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+class Solution:
+    def isValid(self, s: str) -> bool:
+        val_map=defaultdict(str)
+        val_map["("]=")"
+        val_map["["]="]"
+        val_map["{"]="}"
+
+        stack=[]
+        for char in s:
+            stack.append(char)
+            if len(stack)>1 and stack[-1]==val_map[stack[-2]]:       
+                stack.pop(),stack.pop()
+
+        if len(stack)>0:
+            return False
+
+        return True
