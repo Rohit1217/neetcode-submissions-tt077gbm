0567770e554d300174656d98d2853553e19d5705
Rohit Singh <156054570+Rohit1217@users.noreply.github.com>
2026-04-01 17:54:46 +0530

    Update: minimum-stack - submission-7

--- Data Structures & Algorithms/minimum-stack/submission-7.py
+++ Data Structures & Algorithms/minimum-stack/submission-7.py
@@ -1,29 +1,19 @@
-from collections import deque,defaultdict
 class MinStack:
-
     def __init__(self):
-        self.stack=deque()
-        self.monotonic_stack=deque()
+        self.stack = [] # Stores tuples of (value, current_min)
 
     def push(self, val: int) -> None:
-        self.stack.append(val)
-
-        if len(self.monotonic_stack)==0 or self.monotonic_stack[-1]>=val:
-           self.monotonic_stack.append(val)
-
+        if not self.stack:
+            self.stack.append((val, val))
+        else:
+            current_min = self.stack[-1][1]
+            self.stack.append((val, min(val, current_min)))
 
     def pop(self) -> None:
-        val=self.stack[-1]
-        if self.stack[-1]==self.monotonic_stack[-1]:
-            self.monotonic_stack.pop()
-
-        out=self.stack.pop()        
-        return out
+        self.stack.pop()
 
     def top(self) -> int:
-        return self.stack[-1]
-        
+        return self.stack[-1][0]
 
     def getMin(self) -> int:
-        return self.monotonic_stack[-1]
-        
+        return self.stack[-1][1]
