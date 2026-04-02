c0153962c41eafde72cb6ea2b5a96e14da1c2e40
Rohit Singh <156054570+Rohit1217@users.noreply.github.com>
2026-04-02 17:47:20 +0530

    Add: car-fleet - submission-11

---
+++ Data Structures & Algorithms/car-fleet/submission-11.py
@@ -0,0 +1,19 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        
+        fleet_stack=[]
+        pos_speed=list(zip(position,speed))
+        pos_speed.sort(reverse=True)
+
+        print(pos_speed)
+        for idx in range(len(pos_speed)):
+            time_to_reach=max(0,(target-pos_speed[idx][0])/pos_speed[idx][1])
+
+            if fleet_stack and fleet_stack[-1]>=time_to_reach:
+                time_to_reach=fleet_stack[-1]
+                fleet_stack.pop()
+            
+            fleet_stack.append(time_to_reach)
+
+        return len(fleet_stack)
+            
