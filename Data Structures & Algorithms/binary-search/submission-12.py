3d69f6c6a1e010fa7322777fa08af4463db8195c
Rohit Singh <156054570+Rohit1217@users.noreply.github.com>
2026-04-19 02:08:08 +0530

    Add: binary-search - submission-12

---
+++ Data Structures & Algorithms/binary-search/submission-12.py
@@ -0,0 +1,21 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:        
+        left,right=0,len(nums)-1
+
+        while (left<right):
+            search_idx=(left+right)//2
+
+            print(left,right,search_idx)
+            
+            if nums[search_idx]==target:
+                return search_idx
+            elif nums[search_idx]<target:
+                left=search_idx+1
+            else:
+                right=search_idx-1
+
+        if nums[left]==target:
+            return left
+
+        return -1    
+
