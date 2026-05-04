2839321730a50134d8f56f64d0d0c4e7a2eb5fb8
Rohit Singh <156054570+Rohit1217@users.noreply.github.com>
2026-05-04 12:20:45 +0530

    Add: search-2d-matrix - submission-14

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-14.py
@@ -0,0 +1,25 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows,cols=len(matrix),len(matrix[0])
+
+        left,right=0,rows*cols-1
+
+        while left<right:
+            mid=(left+right)//2
+            r_idx,c_idx=mid//cols,mid%cols
+
+            guess=matrix[r_idx][c_idx]
+
+            if guess==target:
+                return True
+            elif guess<target:
+                left=mid+1
+            else:
+                right=right-1
+        
+        print(left,right)
+        r_idx,c_idx=left//cols,left%cols
+        if matrix[r_idx][c_idx]==target:
+            return True
+        
+        return False
