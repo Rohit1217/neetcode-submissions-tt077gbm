c62f3ce331b37ba1602c3ad1a2af99bc7a217cfd
Rohit Singh <156054570+Rohit1217@users.noreply.github.com>
2026-07-29 20:09:32 +0530

    Add: course-schedule - submission-18

---
+++ Data Structures & Algorithms/course-schedule/submission-18.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj_list=[[] for i in range(numCourses)]
+        indegree_count=[0 for i in range(numCourses)]
+
+        for crs,preq in  prerequisites:
+            adj_list[crs].append(preq)
+            indegree_count[preq]+=1
+
+        queue=deque([])
+        for crs in range(numCourses):
+            if indegree_count[crs]==0:
+                queue.append(crs)
+        count=0
+
+        while queue:
+            curr_crs=queue.popleft()
+            count+=1
+
+            for crs in adj_list[curr_crs]:
+                indegree_count[crs]-=1
+
+                if indegree_count[crs]==0:
+                    queue.append(crs)
+            
+        if count!=numCourses:
+            return False
+        
+        return True
+
+
