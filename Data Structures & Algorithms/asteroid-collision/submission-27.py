12c0de7ee03fcabd7cd89a6102442e7d43975a03
Rohit Singh <156054570+Rohit1217@users.noreply.github.com>
2026-08-02 17:18:12 +0530

    Update: asteroid-collision - submission-27

--- Data Structures & Algorithms/asteroid-collision/submission-27.py
+++ Data Structures & Algorithms/asteroid-collision/submission-27.py
@@ -1,21 +1,25 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        stack = []
         
-        for a in asteroids:
-            # We only care about collisions if stack top is moving RIGHT (+)
-            # and current asteroid is moving LEFT (-)
-            while stack and a < 0 < stack[-1]:
-                if stack[-1] < -a:
-                    stack.pop()
-                    continue # Current asteroid 'a' survives, check next stack element
-                elif stack[-1] == -a:
-                    stack.pop() # Both explode
-                # If stack[-1] > -a, the current asteroid 'a' explodes
-                break 
-            else:
-                # This 'else' belongs to the 'while' loop. 
-                # It only runs if the while loop finished WITHOUT a 'break'.
-                stack.append(a)
+        def get_sign(asteroid):
+            return (asteroid>0)
+
+        stack=[]
+        i=1
+        n=len(asteroids)
+
+        for asteroid in asteroids:
+            stack.append(asteroid)
+
+            while len(stack)>1 and (get_sign(stack[-1])==False and get_sign(stack[-2])==True):
+                asteroid1,asteroid2=stack.pop(),stack.pop()
+
+                if abs(asteroid2)==abs(asteroid1):
+                    continue
                 
+                elif abs(asteroid2)>abs(asteroid1):
+                    stack.append(asteroid2)
+                else:
+                    stack.append(asteroid1)
+
         return stack
