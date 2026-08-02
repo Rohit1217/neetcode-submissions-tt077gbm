3cfd77ef6e73bbdbda05f5a2885cb4098d6e5846
Rohit Singh <156054570+Rohit1217@users.noreply.github.com>
2026-08-02 18:24:00 +0530

    Add: last-stone-weight-ii - submission-8

---
+++ Data Structures & Algorithms/last-stone-weight-ii/submission-8.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        n=len(stones)
+        stones.sort()
+        target=sum(stones)//2
+
+        dp=[False for _ in range(target+1)]
+        dp[0]=True
+
+        # print(target)
+
+        for stone in stones:
+            for j in range(target,stone-1,-1):
+                dp[j]=dp[j] or dp[j-stone]
+        
+        for i in range(target,-1,-1):
+            if dp[i]==True:
+                return sum(stones)-2*i
