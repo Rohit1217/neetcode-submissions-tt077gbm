539ee71cd8088835818c6ea49b28f11bd0fa696a
Rohit Singh <156054570+Rohit1217@users.noreply.github.com>
2026-09-13 17:15:02 +0530

    Add: eating-bananas - submission-46

---
+++ Data Structures & Algorithms/eating-bananas/submission-46.py
@@ -0,0 +1,37 @@
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        n=len(piles)
+
+        def timetoFinish(k):
+            total_time=0
+            for i in range(n):
+                total_time+=math.ceil(piles[i]/k)
+
+            return total_time        
+
+        left=1
+        right=max(piles)+1
+
+        # ans in range [left,right)
+
+        if h<len(piles):
+            return -1
+        
+        # Either answer in arr or answer among one last left,right
+        while left<right: #condition when no valid index left (left,left) form
+            mid=(left+right)//2
+            
+
+            time_mid=timetoFinish(mid)
+            
+            if time_mid>h:
+                left=mid+1 #answer in left half [mid+1,right)
+            else:
+                right=mid #Answer in [
+
+        # print(left,right)
+
+        return left
+
