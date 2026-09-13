96e269c09024cef282d896ab612c92719cedf45d
Rohit Singh <156054570+Rohit1217@users.noreply.github.com>
2026-09-13 22:56:34 +0530

    Add: sliding-window-maximum - submission-10

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-10.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        #dequeu monotonic
+
+        n=len(nums)
+        monotonic_queue=deque()
+
+        def push_queue(i,val):
+
+            if monotonic_queue and  monotonic_queue[0][0]<=i-k:
+                monotonic_queue.popleft()
+            
+            while monotonic_queue and monotonic_queue[-1][-1]<val:
+                monotonic_queue.pop()
+            
+            monotonic_queue.append((i,val))
+        
+        res=[]
+
+        for j in range(k-1):
+            push_queue(j,nums[j])
+        
+        j=k-1
+        res=[]
+
+        # print(monotonic_queue,j)
+
+        while j<n:
+            push_queue(j,nums[j])
+            res.append(monotonic_queue[0][-1])
+            j+=1
+        
+        return res
+
+            
+
+
+
+
